ab74e4a81b75da127cdb59aeeeef5a4e7a3ccfae
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-02-02 14:19:32 +0200

    feat: add function code

--- src/index.js
+++ src/index.js
@@ -34,11 +34,39 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+    '-----': '0',
+    ' ': ' '
 };
 
 function decode(expr) {
     // write your solution here
+    let decodedString = '';
+
+    let splitedString = [];
+    for(let i = 0; i < expr.length; i += 10) {
+        splitedString.push(expr.substr(i, 10));
+    }
+
+    splitedString.forEach((elem) => {
+        let morseString = '';
+        
+        if (elem === '**********') {
+            morseString += ' ';
+        } else {
+            for (let i = 0; i < elem.length; i += 2) {
+                if (elem.charAt(i) + elem.charAt(i + 1) === '10') {
+                    morseString += '.';
+                } else if (elem.charAt(i) + elem.charAt(i + 1) === '11') {
+                    morseString += '-';
+                }
+            }
+        }
+        
+        decodedString += MORSE_TABLE[morseString];
+    })
+
+    return decodedString;
+
 }
 
 module.exports = {
